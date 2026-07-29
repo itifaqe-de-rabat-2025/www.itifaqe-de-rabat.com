e2c24e094b73dd043153e947740a315c6699ba7e
itifaqe.de.rabat.2025 <itifaqe.de.rabat.contact@gmail.com>
2026-07-29 05:17:57 +0100

    Add function to toggle menu display

---
+++ script.js
@@ -0,0 +1,17 @@
+function openMenu(){
+
+let menu=document.getElementById("menu");
+
+if(menu.style.display==="block"){
+
+menu.style.display="none";
+
+}
+
+else{
+
+menu.style.display="block";
+
+}
+
+}
