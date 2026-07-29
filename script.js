d387227cd1a6103e2907dd6ccf75df941b0aa7cc
itifaqe.de.rabat.2025 <itifaqe.de.rabat.contact@gmail.com>
2026-07-29 05:41:00 +0100

    Fix formatting and spacing in openMenu function

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function openMenu(){
 
-let menu=document.getElementById("menu");
+let menu = document.getElementById("menu");
 
-if(menu.style.display==="block"){
+if(menu.style.display === "block"){
 
-menu.style.display="none";
+menu.style.display = "none";
 
 }
-  
+
 else{
 
-menu.style.display="block";
+menu.style.display = "block";
 
 }
 
